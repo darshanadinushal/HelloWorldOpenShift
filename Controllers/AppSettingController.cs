9c487b50f4bbb8dd867226b06089e36e6e22bbd6
Darshana Dinushal <darshanadinushal@gmail.com>
2020-12-16 10:38:10 +0800

    add config to appsetting json

---
+++ Controllers/AppSettingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldOpenShift.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppSettingController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public AppSettingController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string dbConn = configuration.GetSection("MySettings").GetSection("DbConnection").Value;
+
+            return Ok(dbConn);
+        }
+    }
+}
